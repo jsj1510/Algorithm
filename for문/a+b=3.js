ae7bf872c972f40fe1b8016edd99f01cf78007ca
jsj <tw450497@gmail.com>
2021-02-06 00:47:43 +0900

    implement: a+b-3

---
+++ for문/a+b=3.js
@@ -0,0 +1,14 @@
+function solution(n, m) {
+    const count = 5;
+    let sum = 0;
+    for (let i = 0; i < count; i++) {
+        return sum = n + m;
+        
+    }
+}
+
+console.log(solution(1, 1));
+console.log(solution(2, 3));
+console.log(solution(3, 4));
+console.log(solution(9, 8));
+console.log(solution(5, 2));
