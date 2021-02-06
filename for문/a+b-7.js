590309b0e61814e47df06443194276b8e84c5d5a
jsj <tw450497@gmail.com>
2021-02-06 18:05:49 +0900

    implement: a + b -7

---
+++ for문/a+b-7.js
@@ -0,0 +1,14 @@
+function solution(n, m) {
+    const count = 5;
+    let sum = 0;
+    for (let i = 0; i < count; i++) {
+        return sum = n + m;
+        
+    }
+}
+
+console.log(`case 1#: ${solution(1, 1)}`);
+console.log(`case 2#: ${solution(14, 1)}`);
+console.log(`case 3#: ${solution(42, 1)}`);
+console.log(`case 4#: ${solution(4, 1)}`);
+console.log(`case 5#: ${solution(11, 1)}`);
