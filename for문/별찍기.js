567759bfab3e946b8fe4af58ee6901b9a5587956
jsj <tw450497@gmail.com>
2021-02-06 18:17:56 +0900

    implement: 별찍기

---
+++ for문/별찍기.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    let result = '';
+    let space = '\n';
+    for (let i = 0; i < n; i++) {
+        result += '*';
+        space;
+        console.log(result);
+    }
+}
+
+solution(5);
