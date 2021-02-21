efa77ff2dd0f2214a3d67afc08f15ede0bbf6d78
jsj <tw450497@gmail.com>
2021-02-22 01:06:04 +0900

    add:다이얼

---
+++ 문자열/다이얼.js
@@ -0,0 +1,12 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+const n = input[0].split(' ');
+
+let time = 0;
+for (let i in n) {
+    if (n[i] ){
+    // 하드코딩 
+
+    }
+}
