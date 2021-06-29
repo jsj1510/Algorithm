825cb94862342ca364854a5ca386ba89ec4eb8c3
unknown <tw450497@gmail.com>
2021-06-29 23:51:14 +0900

    브루트포스/덩치

--- 브루트포스/덩치.js
+++ 브루트포스/덩치.js
@@ -2,10 +2,11 @@ const [n, ...arr] = require("fs").readFileSync("/dev/stdin")
   .toString()
   .trim()
   .split("\n");
-const men = arr.map((v, i) => ({weight: v.split(" ")[0], height: v.split(" ")[1], rank: 1}));
-for (let i = 0; i < men.length; i++) {
-    for (let j = 0; j < men.length; j++) {
-        if (i !== j && men[i].weight < men[j].weight && men[i].height < men[j].height) men[i].rank++;
+const person = arr.map((v, i) => ({weight: v.split(" ")[0], height: v.split(" ")[1], rank: 1}));
+
+for (let i = 0; i < person.length; i++) {
+    for (let j = 0; j < person.length; j++) {
+        if (i !== j && person[i].weight < person[j].weight && person[i].height < person[j].height) person[i].rank++;
     }
 }
-console.log(men.map(i => i.rank).join(" "));
+console.log(person.map(i => i.rank).join(" "));
